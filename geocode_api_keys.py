98b9d9366059d364112d04d2b9f31ef74ff41bd8
WebBreacher <WebBreacher@users.noreply.github.com>
2018-10-11 10:50:20 -0400

    test gitignore

--- geocode_api_keys.py
+++ geocode_api_keys.py
@@ -1,4 +1,5 @@
 import os
 
+# You need to get one or more of these API keys from Google and Bing
 os.environ['GOOGLE_API_KEY'] = 'api_key_from_google_cloud_platform'
 bing_api_key = 'api_key_from_bing'

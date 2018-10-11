5fe6ffdd68444bc19b06185f3433428fed4814dc
WebBreacher <WebBreacher@users.noreply.github.com>
2018-10-11 10:42:01 -0400

    reverting content and testing gitignore

--- geocode_api_keys.py
+++ geocode_api_keys.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ['GOOGLE_API_KEY'] = 'testaddoffakekey'
-bing_api_key = 'testaddoffakekey'
+os.environ['GOOGLE_API_KEY'] = 'api_key_from_google_cloud_platform'
+bing_api_key = 'api_key_from_bing'
